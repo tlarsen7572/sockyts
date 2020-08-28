3c94415149e5b94f3a775f7a1240f5d73f517e2a
Tom <larsenthomasj@gmail.com>
2020-08-28 06:15:39 -0400

    upgrade connections to websockets

--- client/client.go
+++ client/client.go
@@ -1,11 +1,22 @@
 package client
 
-import "net/http"
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
 
 type Server interface {
 	ConnectClient(addressName string, endpointName string) (<-chan string, chan<- string, error)
 }
 
 func SpinUpClient(server Server, w http.ResponseWriter, r *http.Request) {
-
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	_, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,1 +1,55 @@
-package client
+package client_test
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/tlarsen7572/sockyts/client"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+type server struct{}
+
+func (s *server) ConnectClient(addressName string, endpointName string) (<-chan string, chan<- string, error) {
+	return nil, nil, nil
+}
+
+func TestConnectWithoutWebsocketProtocol(t *testing.T) {
+	server := &server{}
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		client.SpinUpClient(server, w, r)
+	}))
+	defer mockServer.Close()
+
+	response, err := http.Get(mockServer.URL)
+	if err != nil {
+		t.Fatalf(`expected no error but got: %v`, err.Error())
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatalf(`expected no error but got: %v`, err.Error())
+
+	}
+	if response.StatusCode != 400 {
+		t.Fatalf(`expected response 400 but got %v`, response.StatusCode)
+	}
+	t.Logf(`%v`, string(body))
+}
+
+func TestConnect(t *testing.T) {
+	server := &server{}
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		client.SpinUpClient(server, w, r)
+	}))
+	defer mockServer.Close()
+
+	u := url.URL{Scheme: "ws", Host: mockServer.URL[7:], Path: ``}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		t.Fatalf(`expected no error but got: %v`, err.Error())
+	}
+	defer conn.Close()
+
+}
